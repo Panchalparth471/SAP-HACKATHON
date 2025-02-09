f2e936ee7982055d9e50632ceebfb91b8bc52c50
Panchalparth471 <panchalparth471@gmail.com>
2025-02-09 09:42:41 +0530

    FrontEnd added

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import PrescriptionScreen from './screens/PrescriptionScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ChatBotScreen from './screens/ChatBotScreen';
+import Scan from './screens/Scan';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen';
 
+
+const stack = createStackNavigator();
 export default function App() {
+  
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+        <stack.Screen name="Home" component={HomeScreen}></stack.Screen>
+        <stack.Screen name="Profile" component={ProfileScreen}></stack.Screen>
+        <stack.Screen name="prescriptionDetails" component={PrescriptionScreen}></stack.Screen>
+        <stack.Screen name="chatBot" component={ChatBotScreen}></stack.Screen>
+        <stack.Screen name="scan" component={Scan}></stack.Screen>
+        <stack.Screen name="login" component={LoginScreen}></stack.Screen>
+        <stack.Screen name="signup" component={SignupScreen}></stack.Screen>
+        <stack.Screen name="forgot" component={ForgotPasswordScreen}></stack.Screen>
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/ChatBotScreen.js
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+export default function ChatBotScreen() {
+    return (
+         <View>
+      <Text>ChatBot Screen</Text>
+    </View>
+    )
+}
+
